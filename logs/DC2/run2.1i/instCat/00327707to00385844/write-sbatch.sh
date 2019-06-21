472844c77bb09acaa9d3f983c14574fbac2ce5c0
desc Pseudo User <desc@cori06.nersc.gov>
2019-06-21 10:28:52 -0700

    instCat 00327707to00385844

---
+++ logs/DC2/run2.1i/instCat/00327707to00385844/write-sbatch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+FILE=$PWD/xfer-$1.sh
+
+/bin/cat <<EOM >$FILE
+#!/bin/bash -l
+#SBATCH -M escori
+#SBATCH -q xfer
+#SBATCH -t 10:00:00
+#SBATCH -J dc2_instCat
+#SBATCH -L projecta,SCRATCH
+#SBATCH --mem=10GB
+cd /global/projecta/projectdirs/lsst/production
+EOM
+
+
+echo "htar -Hcrc -cvf /home/projects/desc/DC2_ImSim/Run2.1i/instCat/00327707to00385844/$2.tar -L /global/cscratch1/sd/desc/HPSS/dc2/run2.1i/instCat/$2.txt" >> $FILE
+
